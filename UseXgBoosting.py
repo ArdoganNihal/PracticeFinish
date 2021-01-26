fac6dfc41546aa98df810d1ec0a893b81cde5757
G. Nihal Ardoğan <ardogannihal@hotmail.com>
2021-01-26 15:56:17 +0300

    Adding files in project 'Practice' to Git...

---
+++ UseXgBoosting.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+import numpy as np
+
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+df= pd.read_csv('C:\DataSet2\Traininig.csv')
+df=df.drop(labels=['rn'], axis=1);
+#print(df.head())
+#print(df.shape)
+y=df[['activity']]
+x=df.iloc[:,1:]
+#print(y)
+x=x.sample(n=20, axis=1, random_state=1)
+print(x.shape)
+from sklearn.model_selection import train_test_split, GridSearchCV
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size=0.30, random_state=42)
